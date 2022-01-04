4e811602fb101926737e18fef7b0ab84e3a87a86
Margaret <mpalaciosgallardo@gmail.com>
2022-01-04 12:07:13 -0500

    Avance de diseño de cards

--- app.js
+++ app.js
@@ -2,22 +2,26 @@
 
 fetch('https://rickandmortyapi.com/api/character/')
   .then(response => response.json())
-  .then(data => data.results.forEach(
-      result => document.querySelector(".card-container").innerHTML += `
-      <div class="card">
+  .then(data => data.results.forEach( function (element, index, array) {
+    console.log(element);  
+    let html = `
+      <article class="card">
             <div class="box1">
-                <img src="${result.image}">
+                <img src="${element.image}">
             </div>
             <div class="box2">
-                <p>
-                    <span>${result.name}</span>
-                    <span>Status: ${result.status}</span>
-                    <span>Specie: ${result.species}</span>
-                    <span>Type: ${result.type}</span>
-                    <span>Gender: ${result.gender}</span>
-                    <span>Origin Name: ${result.location.name}</span>
-                </p>
+                <span>${element.name}</span> 
+                <div>`;
+    if (element.status == 'unknown') { html += '<span class="circle unknown"></span>'}
+    else if (element.status == 'Alive') {html +='<span class="circle alive"></span>'}
+    else {html +='<span class="circle dead"></span>'}
+    html +=`<span>${element.status} - ${element.species}</span></div>
+                <span>${element.gender}</span>
+                <span>From ${element.location.name}</span>
             </div>
-      </div>`))
+      </article>`;
+    document.querySelector(".card-container").innerHTML += html; }))
     .catch(error => console.log(error));
+      
+    
 
