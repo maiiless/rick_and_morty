a495ebcda306679e6e110a71cc83e6083a695f8d
Margaret <mpalaciosgallardo@gmail.com>
2022-01-03 22:17:28 -0500

    Cambios de diseño

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ fetch('https://rickandmortyapi.com/api/character/')
   .then(data => data.results.forEach(
       result => document.querySelector(".card-container").innerHTML += `
       <div class="card">
-            <img src="${result.image}">
-            <p>
-                <span>Name: ${result.name}</span>
-                <span>Status: ${result.status}</span>
-                <span>Specie: ${result.species}</span>
-                <span>Type: ${result.type}</span>
-                <span>Gender: ${result.gender}</span>
-                <span>Origin Name: ${result.location.name}</span>
-            </p>
+            <div class="box1">
+                <img src="${result.image}">
+            </div>
+            <div class="box2">
+                <p>
+                    <span>${result.name}</span>
+                    <span>Status: ${result.status}</span>
+                    <span>Specie: ${result.species}</span>
+                    <span>Type: ${result.type}</span>
+                    <span>Gender: ${result.gender}</span>
+                    <span>Origin Name: ${result.location.name}</span>
+                </p>
+            </div>
       </div>`))
     .catch(error => console.log(error));
 
